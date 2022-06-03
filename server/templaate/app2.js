efc07a4bde8f1ab1b0f8408a8002b4c2990ef2bf
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2022-06-03 18:56:57 +0800

    delete files | move to example

--- server/templaate/app2.js
+++
@@ -1,8 +0,0 @@
-
-const express = require("express");
-const app = express();
-const myRoute = require("./myRoute.js");
-
-app.use("/home", myRoute); // 將Router模組掛載到'/home底下'
-
-app.listen(3011, () => console.log(3011));
